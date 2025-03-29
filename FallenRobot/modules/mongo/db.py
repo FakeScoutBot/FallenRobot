cf830ece99c6222177c476e103479756f02c7e4b
FakeScoutBot <132735283+FakeScoutBot@users.noreply.github.com>
2025-03-30 00:50:08 +0530

    Delete FallenRobot/modules/mongo directory
    Signed-off-by: FakeScoutBot <132735283+FakeScoutBot@users.noreply.github.com>

--- FallenRobot/modules/mongo/db.py
+++
@@ -1,8 +0,0 @@
-from pymongo import MongoClient
-from FallenRobot import MONGO_DB_URI
-
-client = MongoClient(MONGO_DB_URI)
-db = client.FallenRobot
-
-# Collections
-karma = db.karma  # Karma collection
